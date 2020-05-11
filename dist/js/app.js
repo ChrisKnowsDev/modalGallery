5c09775572a953e6a356ff6765d5b7943530c61b
ChrisKnowsDev <chrisharley81@gmail.com>
2020-05-11 19:39:29 -0400

    created Gallery function and called it on both galleries, nested a openModal function inside to open modal on image click

---
+++ dist/js/app.js
@@ -0,0 +1,43 @@
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error('No gallery found!');
+  }
+
+  // select the elements we need
+  const images = Array.from(gallery.querySelectorAll('img'));
+  console.log(images);
+  const modal = document.querySelector('.modal');
+  const prevBtn = modal.querySelector('.prev');
+  const nextBtn = modal.querySelector('.next');
+  let currentImage;
+
+  // open modal
+  function openModal() {
+    console.info('Opening modal!');
+    // check to see if modal is already open
+    if (modal.matches('.open')) {
+      console.info('modal already open');
+    }
+    modal.classList.add('open');
+  }
+
+  // show images
+  function showImage(el) {
+    if (!el) {
+      console.info('no image to show');
+    }
+    // update the modal with this info
+    modal.querySelector('img').src = el.src;
+    modal.querySelector('h2').textContent = el.title;
+    modal.querySelector('p').textContent = el.dataset.description;
+    currentImage = el;
+    openModal();
+  }
+
+  images.forEach(image =>
+    image.addEventListener('click', e => showImage(e.currentTarget))
+  );
+}
+
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
